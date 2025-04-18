cb0bdc19afe1e93e9d0f746d2558cec784b3ee40
monojoycodes <24deym@rbunagpur.in>
2025-04-18 14:24:02 +0530

    object-oriented-programming

---
+++ exp-1_classesAndObjects/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        exp1 myCar = new exp1();
+        myCar.color = "Red";
+        myCar.speed = 100;
+        myCar.displayInfo();
+    }
+}
---
+++ exp-1_classesAndObjects/src/exp1.java
@@ -0,0 +1,9 @@
+public class exp1 {
+        String color;
+        int speed;
+
+        void displayInfo() {
+            System.out.println("Color: " + color);
+            System.out.println("Speed: " + speed + " km/h");
+        }
+    }
